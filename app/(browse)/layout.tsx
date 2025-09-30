5c6ba5fe1d527e332eab99797f916d290973c836
Yannis <yannis.teissier70@gmail.com>
2025-09-30 19:15:10 +0200

    Sidebar

--- app/(browse)/layout.tsx
+++ app/(browse)/layout.tsx
@@ -1,11 +1,16 @@
 import {Navbar} from "@/app/(browse)/_components/navbar";
+import {Sidebar} from "@/app/(browse)/_components/sidebar";
+import {Container} from "@/app/(browse)/_components/container";
 
 const BrowseLayout = ({children}: { children: React.ReactNode }) => {
     return (
         <>
             <Navbar />
             <div className="flex h-full pt-20">
-                {children}s
+                <Sidebar />
+                <Container>
+                    {children}
+                </Container>
             </div>
         </>
     )
