d6581921cc7149db85aad3a140b392486f6fb092
Yannis <yannis.teissier70@gmail.com>
2025-10-02 15:15:05 +0200

    Add chat settings

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,9 +1,9 @@
 export const runtime = "nodejs";
 
-import { verifyWebhook } from '@clerk/nextjs/webhooks'
-import { NextRequest } from 'next/server'
+import {verifyWebhook} from '@clerk/nextjs/webhooks'
+import {NextRequest} from 'next/server'
 
-import { db } from '@/lib/db'
+import {db} from '@/lib/db'
 
 export async function POST(req: NextRequest) {
     try {
@@ -28,7 +28,7 @@ export async function POST(req: NextRequest) {
                 break;
             case "user.updated":
                 await db.user.update({
-                    where: { externalUserId: evt.data.id },
+                    where: {externalUserId: evt.data.id},
                     data: {
                         username: evt.data.username,
                         imageUrl: evt.data.image_url,
@@ -37,7 +37,7 @@ export async function POST(req: NextRequest) {
                 break;
             case "user.deleted":
                 await db.user.delete({
-                    where: { externalUserId: evt.data.id }
+                    where: {externalUserId: evt.data.id}
                 });
                 break;
             default:
@@ -45,9 +45,9 @@ export async function POST(req: NextRequest) {
 
         }
 
-        return new Response('Webhook received', { status: 200 })
+        return new Response('Webhook received', {status: 200})
     } catch (err) {
         console.error('Error verifying webhook:', err)
-        return new Response('Error verifying webhook', { status: 400 })
+        return new Response('Error verifying webhook', {status: 400})
     }
 }
